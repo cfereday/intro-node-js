cae4ee54bde885a4026fae40dc25d83be45b1eff
Charlotte Fereday <charlotte.fereday@tesglobal.com>
2020-07-17 12:55:56 +0200

    Changes server to be async & not crash

--- exercises/api/server.js
+++ exercises/api/server.js
@@ -1,40 +1,61 @@
-const http = require('http')
-const url = require('url')
-const fs = require('fs')
-const path = require('path')
+const http = require('http');
+const url = require('url');
+const fs = require('fs');
+const path = require('path');
 
-/**
- * this function is blocking, fix that
- * @param {String} name full file name of asset in asset folder
- */
-const findAsset = (name) => {
-  const assetPath = path.join(__dirname, 'assets', name)
-  return fs.readFileSync(assetPath, {encoding: 'utf-8'}).toString()
-}
 
-const hostname = '127.0.0.1'
-const port = 3000
+const findAssetAsync = (name) => {
+  const assetPath = path.join(__dirname, 'assets', name);
+  return new Promise((resolve, reject) => {
+        fs.readFile(assetPath, {encoding: 'utf-8'}, (err, asset) => {
+          if (err) {
+            reject(err);
+          }
+
+          if (asset) {
+            resolve(asset);
+          }
+        })
+      }
+  );
+};
+
+const hostname = '127.0.0.1';
+const port = 3000;
 
 // log incoming request coming into the server. Helpful for debugging and tracking
-const logRequest = (method, route, status) => console.log(method, route, status)
+const logRequest = (method, route, status) => console.log(method, route, status);
+
+const getTheAssets = async (method, route, res, name, status) => {
+  try {
+    res.writeHead(status, {'Content-Type': 'text/html'});
+    const assets = await findAssetAsync(name);
+    res.write(assets);
+    logRequest(method, route, status);
+    return res.end;
+  } catch (err) {
+    console.log('Something went wrong:', err);
+    res.writeHead(500, {'Content-Type': 'text/html'});
+    const assets = await findAssetAsync('sorry-something-went-wrong.html');
+    res.write(assets);
+    logRequest(method, route, 500);
+    return res.end;
+  }
+};
 
 const server = http.createServer((req, res) => {
-  const method = req.method
-  const route = url.parse(req.url).pathname
+  const method = req.method;
+  const route = url.parse(req.url).pathname;
   // this is sloppy, especially with more assets, create a "router"
   if (route === '/') {
-    res.writeHead(200, {'Content-Type': 'text/html'})
-    res.write(findAsset('index.html'))
-    logRequest(method, route, 200)
-    res.end()
-  } else {
-    // missing asset should not cause server crash
-    throw new Error('route not found')
-    res.end()
+    return getTheAssets(method, route, res, 'index.html', 200);
   }
-  // most important part, send down the asset
-})
+
+  if (route !== '/') {
+    return getTheAssets(method, route, res, 'page-not-found.html', 404)
+  }
+});
 
 server.listen(port, hostname, () => {
   console.log(`Server running at http://${hostname}:${port}/`)
-})
+});
